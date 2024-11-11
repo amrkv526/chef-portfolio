06da5731bf28a67502661837bd82b64f2f9d2e19
amrkv526 <93342376+amrkv526@users.noreply.github.com>
2024-11-11 08:57:32 -0500

    created header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './app.css';
 import Home from './pages/home';
 import About from './pages/about';
 import Recipes from './pages/recipes';
 import Recipe from './pages/recipe';
 import Contact from './pages/contact';
+import Header from './components/header';
 
 function App() {
   return (
     <Router>
+      <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
---
+++ src/components/header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../app.css';
+
+function Header() {
+  return (
+    <header className="header-container">
+      <nav className="header-nav">
+        
+        <div className="header-links left-links">
+          <a href="/about">About</a>
+          <a href="/recipes">Recipes</a>
+          <a href="/blog">Blog</a>
+        </div>
+        
+        <h1 className="header-title">Chef Dionne Reid</h1>
+        
+        <div className="header-links right-links">
+          <a href="/events">Events</a>
+          <a href="/shop">Shop</a>
+          <a href="/contact">Contact</a>
+          <img src="/images/shopping-cart.png" alt="Shopping cart" className="shopping-cart" />
+          <button className="sign-up-button">Sign Up</button>
+        </div>
+        
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
