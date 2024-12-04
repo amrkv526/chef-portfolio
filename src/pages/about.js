0d35a1a07972dac5ad05f1a24935a15ccd0d0b8d
amrkv526 <93342376+amrkv526@users.noreply.github.com>
2024-12-04 07:00:31 -0500

    added about page styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,25 +2,39 @@ import React from 'react';
 
 function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-        A recent transplant from Jamaica, Chef Dionne Reid is a chef-restaurateur turned test cook, 
-        food journalist, and supper club host of OnlyTogether. Her love of contemporary Caribbean 
-        cuisine has woven its way into all her food philosophies and culinary adventures. Food has 
-        brought her to kitchens in Boston and New York, France and now, Toronto. Yay, to new adventures!
-      </p>
-      <div className="about-images">
-        <img 
-          src="/images/chefdionne.jpg" 
-          alt="Chef Dionne Reid Portrait" 
-          className="about-image"
-        />
-        <img 
-          src="/images/chefdionne2.png" 
-          alt="Contemporary Caribbean Dish" 
-          className="about-image"
-        />
+    <div className="about-container">
+      <h1 className="about-title">About</h1>
+      <div className="about-content">
+        <div className="about-row">
+          <p className="about-text">
+            A recent transplant from Jamaica, Chef Dionne Reid is a chef-restaurateur turned test cook.
+          </p>
+          <img 
+            src="/images/chefdionne.jpg" 
+            alt="Chef Dionne Reid Portrait" 
+            className="about-image"
+          />
+        </div>
+        <div className="about-row">
+          <img 
+            src="/images/chefdionne2.png" 
+            alt="Contemporary Caribbean Dish" 
+            className="about-image"
+          />
+          <p className="about-text">
+            Her love of contemporary Caribbean cuisine has woven its way into all her food philosophies and culinary adventures.
+          </p>
+        </div>
+        <div className="about-row">
+          <p className="about-text">
+            Food has brought her to kitchens in Boston and New York, France and now, Toronto. Yay, to new adventures!
+          </p>
+          <img 
+            src="https://via.placeholder.com/150" 
+            alt="Chef Dionne Cooking" 
+            className="about-image"
+          />
+        </div>
       </div>
     </div>
   );
